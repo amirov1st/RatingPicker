d3bba9fa5650e243f3316fcbc31e7e5d6191f3ac
Amirov Foma <amirov.foma@icloud.com>
2024-06-23 11:12:33 +0300

    Initial Commit

---
+++ RatingPicker/RatingPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingPickerApp.swift
+//  RatingPicker
+//
+//  Created by Amirov Foma on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct RatingPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
